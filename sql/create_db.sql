320d49e81afb472c851ab61f04ebeef2e2ddd51e
Paul Coignac <paulcoignac@gmail.com>
2021-09-08 08:31:22 +0200

    initialize create_db sql

---
+++ sql/create_db.sql
@@ -0,0 +1,9 @@
+
+    
+--User(email(1),password(NN),lname(NN),fname(NN),date(NN),sex(NN),height(NN),weight(NN))
+
+--Activity(id(1),user_id=@User.email(NN),date(NN),description(NN))
+
+--ActivityData(activity_id=@Activity.id(NN),time(NN),cardio_frequency(NN),latitude(NN),longitude(NN),altitude(NN))
+
+--
