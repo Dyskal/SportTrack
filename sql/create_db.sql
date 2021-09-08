786aaf0559c4d1d803e0bd265c34c65f93f857fb
Dyskal <yoshilexis@gmail.com>
2021-09-08 08:49:56 +0200

    add sql

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,9 +1,26 @@
-
-    
 --User(email(1),password(NN),lname(NN),fname(NN),date(NN),sex(NN),height(NN),weight(NN))
-
 --Activity(id(1),user_id=@User.email(NN),date(NN),description(NN))
-
 --ActivityData(activity_id=@Activity.id(NN),time(NN),cardio_frequency(NN),latitude(NN),longitude(NN),altitude(NN))
+create table User (
+	email text not null
+		constraint User_pk
+			primary key,
+	password text not null,
+	lname text not null,
+	fname text not null,
+	date text not null,
+	sex text not null,
+	height integer not null,
+	weight integer not null
+);
 
---
+create table Activity (
+	id integer
+		constraint Activity_pk
+			primary key,
+	user_id integer not null
+		constraint Activity_User_fk
+			references User (email),
+	date text not null,
+	description text not null
+);
