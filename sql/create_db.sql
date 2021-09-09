8d11520f42bb2aff902c0d0b392e959b881aefd4
Dyskal <yoshilexis@gmail.com>
2021-09-09 12:13:05 +0200

    update db

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -2,6 +2,11 @@
 --Activity(id(1),user_id=@User.email(NN),date(NN),description(NN))
 --ActivityData(activity_id=@Activity.id(NN),time(NN),cardio_frequency(NN),latitude(NN),longitude(NN),altitude(NN))
 
+PRAGMA foreign_keys = ON;
+DROP TABLE IF EXISTS ActivityData;
+DROP TABLE IF EXISTS Activity;
+DROP TABLE IF EXISTS User;
+
 CREATE TABLE User (
 	email TEXT NOT NULL
 		CONSTRAINT User_pk
@@ -21,20 +26,18 @@ CREATE TABLE Activity (
 	id INTEGER
 		CONSTRAINT Activity_pk
 			PRIMARY KEY,
-	user_id INTEGER NOT NULL
-		CONSTRAINT Activity_User_fk
-			REFERENCES User(email),
+	user_id INTEGER NOT NULL,
 	date TEXT NOT NULL,
-	description TEXT NOT NULL
+	description TEXT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES User(email)
 );
 
 CREATE TABLE ActivityData (
-    activity_id INTEGER NOT NULL
-        CONSTRAINT ActivityData_Activity_fk 
-			REFERENCES Activity(id),
+    activity_id INTEGER NOT NULL,
     time TEXT NOT NULL,
     cardio_frequency INTEGER NOT NULL,
-    latitude INTEGER NOT NULL,
-    longitude INTEGER NOT NULL,
-    altitude INTEGER NOT NULL
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL,
+    altitude INTEGER NOT NULL,
+    FOREIGN KEY(activity_id) REFERENCES Activity(id)
 );
