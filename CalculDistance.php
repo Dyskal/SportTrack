e3702568c85d5d733fb4036ba1dbe87d6b5f501a
Dyskal <yoshilexis@gmail.com>
2021-09-13 23:19:19 +0200

    fixed php UwU

--- CalculDistance.php
+++ CalculDistance.php
@@ -1,8 +1,5 @@
 <?php
 interface CalculDistance {
-
-
-
     /**
      * Retourne la distance en mètres entre 2 points GPS exprimés en degrés.
      * @param float $lat1 Latitude du premier point GPS
@@ -11,15 +8,15 @@ interface CalculDistance {
      * @param float $long2 Longitude du second point GPS
      * @return float La distance entre les deux points GPS
      */
-    public function calculDistance2PointsGPS(float $lat1, float $long1, float $lat2, float $long2): float;
+    public function calculDistance2PointsGPS($lat1, $long1, $lat2, $long2);
 
 
     /**
-     * Retourne la distance en metres du parcours passé en paramètres. Le parcours est
-     * défini par un tableau ordonné de points GPS.
+     * Retourne la distance en metres du parcours passé en paramètres.
+     * Le parcours est défini par un tableau ordonné de points GPS.
      * @param Array $parcours Le tableau contenant les points GPS
      * @return float La distance du parcours
      */
-    public function calculDistanceTrajet(Array $parcours): float;
+    public function calculDistanceTrajet($parcours);
 }
 ?>
--- CalculDistanceImpl.php
+++ CalculDistanceImpl.php
@@ -1,27 +1,31 @@
 <?php
+require("CalculDistance.php");
 class CalculDistanceImpl implements CalculDistance {
 
     /**
      * @inheritDoc
      */
-    public function calculDistance2PointsGPS(float $lat1, float $long1, float $lat2, float $long2): float {
-        $r = 6378.137;
-        $lat1 = deg2rad($lat1);
-        $lat2 = deg2rad($lat2);
-        $long1 = deg2rad($long1);
-        $long2 = deg2rad($long2);
-
-        return $r * acos(sin($lat2)*sin($lat1)+cos($lat2)*cos($lat1)*cos($long2-$long1));
+    public function calculDistance2PointsGPS($lat1, $long1, $lat2, $long2) {
+        return 6378137 * acos(sin(deg2rad($lat2)) * sin(deg2rad($lat1)) + cos(deg2rad($lat2)) * cos(deg2rad($lat1)) * cos(deg2rad($long2) - deg2rad($long1)));
     }
 
     /**
      * @inheritDoc
      */
-    public function calculDistanceTrajet(array $parcours): float {
+    public function calculDistanceTrajet($parcours) {
         $ret = 0.0;
-        for ($i = 0; $i <= count($parcours) - 3; $i++) {
-            $ret += $this->calculDistance2PointsGPS($parcours[0+$i], $parcours[1+$i], $parcours[2+$i], $parcours[3+$i]);
+        for ($i = 1; $i < sizeof($parcours); $i++) {
+            $ret += $this->calculDistance2PointsGPS($parcours[$i-1]['latitude'], $parcours[$i-1]['longitude'], $parcours[$i]['latitude'], $parcours[$i]['longitude']);
         }
         return $ret;
+    }
+
+    public function json_cut($array) {
+        $narray = [];
+        $array = json_decode($array, true);
+        for ($i = 0; $i < sizeof($array); $i++) {
+            $narray += array($i => array('latitude' => $array[$i]['latitude'], 'longitude' => $array[$i]['longitude']));
+        }
+        return $narray;
     }
 }
