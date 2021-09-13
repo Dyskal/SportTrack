1c617b49f107711955e05eba66f1f8f863569d41
Dyskal <yoshilexis@gmail.com>
2021-09-13 12:27:39 +0200

    update CalculDistanceImpl.php

--- CalculDistanceImpl.php
+++ CalculDistanceImpl.php
@@ -1,26 +1,27 @@
 <?php
 class CalculDistanceImpl implements CalculDistance {
 
-    /* racine carree de (xa-xb)^2 + (yb-ya)^2 */
     /**
      * @inheritDoc
      */
     public function calculDistance2PointsGPS(float $lat1, float $long1, float $lat2, float $long2): float {
-        // TODO: Implement calculDistance2PointsGPS() method.
         $r = 6378.137;
         $lat1 = deg2rad($lat1);
         $lat2 = deg2rad($lat2);
         $long1 = deg2rad($long1);
         $long2 = deg2rad($long2);
 
-        $d = $r * arccos(sin($lat2)*sin($lat1)+cos($lat2)*cos($lat1)*cos($long2-$long1));
-        return $d;
+        return $r * acos(sin($lat2)*sin($lat1)+cos($lat2)*cos($lat1)*cos($long2-$long1));
     }
 
     /**
      * @inheritDoc
      */
     public function calculDistanceTrajet(array $parcours): float {
-        // TODO: Implement calculDistanceTrajet() method.
+        $ret = 0.0;
+        for ($i = 0; $i <= count($parcours) - 3; $i++) {
+            $ret += $this->calculDistance2PointsGPS($parcours[0+$i], $parcours[1+$i], $parcours[2+$i], $parcours[3+$i]);
+        }
+        return $ret;
     }
 }
