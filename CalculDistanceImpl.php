a0a6b43f669d4f0944d045e88281ceec113e49dd
Dyskal <yoshilexis@gmail.com>
2021-09-13 11:44:32 +0200

    start php

---
+++ CalculDistanceImpl.php
@@ -0,0 +1,18 @@
+<?php
+class CalculDistanceImpl implements CalculDistance {
+
+    /**
+     * @inheritDoc
+     */
+    public function calculDistance2PointsGPS(float $lat1, float $long1, float $lat2, float $long2): float {
+        // TODO: Implement calculDistance2PointsGPS() method.
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function calculDistanceTrajet(array $parcours): float {
+        // TODO: Implement calculDistanceTrajet() method.
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 [Consignes TP](https://people.irisa.fr/Nicolas.Le_Sommer/ens/M3104/tps/)
+[Site IUT](http://m3104.iut-info-vannes.net/m3104_24/)
