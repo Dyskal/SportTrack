e22abcaa7d391e7415de7d4c31ada5d438d8cc8e
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2021-09-15 09:02:12 +0200

    Update UserDAO.php

--- UserDAO.php
+++ UserDAO.php
@@ -1,5 +1,7 @@
 <?php
 require_once('model/SqliteConnection.php');
+include("User.php");
+
 class UserDAO {
     private static $dao;
 
@@ -24,20 +26,34 @@ class UserDAO {
       if($st instanceof User){
          $dbc = SqliteConnection::getInstance()->getConnection();
          // prepare the SQL statement
-         $query = "insert into students(email, password, lname, fname, bdate, gender) values (:l,:n,:p)";
+         $query = "insert into User(email, password, lname, fname, bdate, gender, height, weight) values (:email, :password, lname, :fname, :bdate, :gender, :height, :weight)";
          $stmt = $dbc->prepare($query);
 
          // bind the paramaters
-         $stmt->bindValue(':l',$st->getLogin(),PDO::PARAM_STR);
-         $stmt->bindValue(':n',$st->getLastname(),PDO::PARAM_STR);
-         $stmt->bindValue(':p',$st->getFirstname(),PDO::PARAM_STR);
+         $stmt->bindValue(':email',$st->getEmail(),PDO::PARAM_STR);
+         $stmt->bindValue(':password',$st->getPassword(),PDO::PARAM_STR);
+         $stmt->bindValue(':lname',$st->getLastname(),PDO::PARAM_STR);
+         $stmt->bindValue(':fname',$st->getFirstname(),PDO::PARAM_STR);
+         $stmt->bindValue(':bdate',$st->getBirtDate(),PDO::PARAM_STR);
+         $stmt->bindValue(':gender',$st->getGender(),PDO::PARAM_STR);
+         $stmt->bindValue(':height',$st->getHeight(),PDO::PARAM_STR);
+         $stmt->bindValue(':weight',$st->getWeight(),PDO::PARAM_STR);
 
          // execute the prepared statement
          $stmt->execute();
      }
   }
 
-  //public function delete($obj){ ... }
+  public function delete($st){ 
+     if($st instanceof User){
+         $dbc = SqliteConnection::getInstance()->getConnection();
+         // prepare the SQL statement
+         $query = "delete from User where email = :email";
+         $stmt = $dbc->prepare($query);
+         $stmt->bindValue(':email',$st->getEmail(),PDO::PARAM_STR);
+         $stmt->execute();
+      }
+  }
 
   //public function update($obj){...}
 }
