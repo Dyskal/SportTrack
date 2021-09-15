74a62a5ac67400aa719d835c8a7c43e7d383e3ac
Dyskal <yoshilexis@gmail.com>
2021-09-15 09:14:58 +0200

    update User/UserDAO

--- User.php
+++ User.php
@@ -1,35 +1,35 @@
 <?php
- class Student{
-   private $lname;
-   private $fname;
-   private $password;
-   private $email;
-   private $bdate;
-   private $gender;
-   private $height;
-   private $weight;
+class User {
+    private $lname;
+    private $fname;
+    private $password;
+    private $email;
+    private $bdate;
+    private $gender;
+    private $height;
+    private $weight;
 
-   public function  __construct() { }
-   public function init($n,$p,$passwd,$email,$bdate,$gender,$height,$weight){
-     $this->lname = $n;
-     $this->fname = $p;
-     $this->password = $passwd;
-     $this->email = $email;
-     $this->bdate = $bdate;
-     $this->gender = $gender;
-     $this->height = $height;
-     $this->weight = $weight;
-   }
+    public function  __construct() { }
+    public function init($n,$p,$passwd,$email,$bdate,$gender,$height,$weight){
+        $this->lname = $n;
+        $this->fname = $p;
+        $this->password = $passwd;
+        $this->email = $email;
+        $this->bdate = $bdate;
+        $this->gender = $gender;
+        $this->height = $height;
+        $this->weight = $weight;
+    }
 
-   public function getLastname(){ return $this->lname; }
-   public function getFirstname(){ return $this->fname; }
-   public function getPassword(){ return $this->password; }
-   public function getEmail(){ return $this->email; }
-   public function getBirthDate(){ return $this->bdate; }
-   public function getGender(){ return $this->gender; }
-   public function getHeight(){ return $this->height; }
-   public function getWeight(){ return $this->weight; }
+    public function getLastname(){ return $this->lname; }
+    public function getFirstname(){ return $this->fname; }
+    public function getPassword(){ return $this->password; }
+    public function getEmail(){ return $this->email; }
+    public function getBirthDate(){ return $this->bdate; }
+    public function getGender(){ return $this->gender; }
+    public function getHeight(){ return $this->height; }
+    public function getWeight(){ return $this->weight; }
 
-   public function  __toString() { return $this->lname. " ". $this->fname. " ". $this->password. " ". $this->email. " ". $this->bdate. " ". $this->gender. " ". $this->height. " ". $this->weight; }
-  }
+    public function  __toString() { return $this->lname. " ". $this->fname. " ". $this->password. " ". $this->email. " ". $this->bdate. " ". $this->gender. " ". $this->height. " ". $this->weight; }
+}
 ?>
--- UserDAO.php
+++ UserDAO.php
@@ -1,80 +1,78 @@
 <?php
 require_once('model/SqliteConnection.php');
 include("User.php");
-
 class UserDAO {
     private static $dao;
 
     private function __construct() {}
 
     public final static function getInstance() {
-       if(!isset(self::$dao)) {
-           self::$dao= new UserDAO();
-       }
-       return self::$dao;
+        if(!isset(self::$dao)) {
+            self::$dao = new UserDAO();
+        }
+        return self::$dao;
     }
 
     public final function findAll(){
-       $dbc = SqliteConnection::getInstance()->getConnection();
-       $query = "select * from user order by lname,fname";
-       $stmt = $dbc->query($query);
-       $results = $stmt->fetchALL(PDO::FETCH_CLASS, 'User');
-       return $results;
+        $dbc = SqliteConnection::getInstance()->getConnection();
+        $query = "Select * From User Order By lname,fname";
+        $stmt = $dbc->query($query);
+        return $stmt->fetchALL(PDO::FETCH_CLASS, 'User');
     }
 
-   public final function insert($st){
-      if($st instanceof User){
-         $dbc = SqliteConnection::getInstance()->getConnection();
-         // prepare the SQL statement
-         $query = "insert into User(email, password, lname, fname, bdate, gender, height, weight) values (:email, :password, lname, :fname, :bdate, :gender, :height, :weight)";
-         $stmt = $dbc->prepare($query);
+    public final function insert($st){
+        if($st instanceof User){
+            $dbc = SqliteConnection::getInstance()->getConnection();
+            // prepare the SQL statement
+            $query = "Insert Into User(email, password, lname, fname, bdate, gender, height, weight) Values (:email, :password, :lname, :fname, :bdate, :gender, :height, :weight)";
+            $stmt = $dbc->prepare($query);
 
-         // bind the paramaters
-         $stmt->bindValue(':email',$st->getEmail(),PDO::PARAM_STR);
-         $stmt->bindValue(':password',$st->getPassword(),PDO::PARAM_STR);
-         $stmt->bindValue(':lname',$st->getLastname(),PDO::PARAM_STR);
-         $stmt->bindValue(':fname',$st->getFirstname(),PDO::PARAM_STR);
-         $stmt->bindValue(':bdate',$st->getBirtDate(),PDO::PARAM_STR);
-         $stmt->bindValue(':gender',$st->getGender(),PDO::PARAM_STR);
-         $stmt->bindValue(':height',$st->getHeight(),PDO::PARAM_STR);
-         $stmt->bindValue(':weight',$st->getWeight(),PDO::PARAM_STR);
+            // bind the paramaters
+            $stmt->bindValue(':email',$st->getEmail(),PDO::PARAM_STR);
+            $stmt->bindValue(':password',$st->getPassword(),PDO::PARAM_STR);
+            $stmt->bindValue(':lname',$st->getLastname(),PDO::PARAM_STR);
+            $stmt->bindValue(':fname',$st->getFirstname(),PDO::PARAM_STR);
+            $stmt->bindValue(':bdate',$st->getBirthDate(),PDO::PARAM_STR);
+            $stmt->bindValue(':gender',$st->getGender(),PDO::PARAM_STR);
+            $stmt->bindValue(':height',$st->getHeight(),PDO::PARAM_STR);
+            $stmt->bindValue(':weight',$st->getWeight(),PDO::PARAM_STR);
 
-         // execute the prepared statement
-         $stmt->execute();
-     }
-  }
+            // execute the prepared statement
+            $stmt->execute();
+        }
+    }
 
-  public function delete($st){ 
-     if($st instanceof User){
-         $dbc = SqliteConnection::getInstance()->getConnection();
-         // prepare the SQL statement
-         $query = "delete from User where email = :email";
-         $stmt = $dbc->prepare($query);
-         $stmt->bindValue(':email',$st->getEmail(),PDO::PARAM_STR);
-         $stmt->execute();
-      }
-  }
+    public function delete($st){
+        if($st instanceof User){
+            $dbc = SqliteConnection::getInstance()->getConnection();
+            // prepare the SQL statement
+            $query = "Delete From User Where email = :email";
+            $stmt = $dbc->prepare($query);
+            $stmt->bindValue(':email',$st->getEmail(),PDO::PARAM_STR);
+            $stmt->execute();
+        }
+    }
 
-  public function update($st){
-      if($st instanceof User){
-         $dbc = SqliteConnection::getInstance()->getConnection();
-         // prepare the SQL statement
-         $query = "Update User SET email = :email , password = :password, lname = :lname, fname = :fname, bdate = :bdate, gender = :gender, height = :height, weight = :weight where email =:email";
-         $stmt = $dbc->prepare($query);
+    public function update($st){
+        if($st instanceof User){
+            $dbc = SqliteConnection::getInstance()->getConnection();
+            // prepare the SQL statement
+            $query = "Update User Set email = :email , password = :password, lname = :lname, fname = :fname, bdate = :bdate, gender = :gender, height = :height, weight = :weight Where email =:email";
+            $stmt = $dbc->prepare($query);
 
-         // bind the paramaters
-         $stmt->bindValue(':email',$st->getEmail(),PDO::PARAM_STR);
-         $stmt->bindValue(':password',$st->getPassword(),PDO::PARAM_STR);
-         $stmt->bindValue(':lname',$st->getLastname(),PDO::PARAM_STR);
-         $stmt->bindValue(':fname',$st->getFirstname(),PDO::PARAM_STR);
-         $stmt->bindValue(':bdate',$st->getBirtDate(),PDO::PARAM_STR);
-         $stmt->bindValue(':gender',$st->getGender(),PDO::PARAM_STR);
-         $stmt->bindValue(':height',$st->getHeight(),PDO::PARAM_STR);
-         $stmt->bindValue(':weight',$st->getWeight(),PDO::PARAM_STR);
+            // bind the paramaters
+            $stmt->bindValue(':email',$st->getEmail(),PDO::PARAM_STR);
+            $stmt->bindValue(':password',$st->getPassword(),PDO::PARAM_STR);
+            $stmt->bindValue(':lname',$st->getLastname(),PDO::PARAM_STR);
+            $stmt->bindValue(':fname',$st->getFirstname(),PDO::PARAM_STR);
+            $stmt->bindValue(':bdate',$st->getBirthDate(),PDO::PARAM_STR);
+            $stmt->bindValue(':gender',$st->getGender(),PDO::PARAM_STR);
+            $stmt->bindValue(':height',$st->getHeight(),PDO::PARAM_STR);
+            $stmt->bindValue(':weight',$st->getWeight(),PDO::PARAM_STR);
 
-         // execute the prepared statement
-         $stmt->execute();
-   }
-  }
+            // execute the prepared statement
+            $stmt->execute();
+        }
+    }
 }
 ?>
