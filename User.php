3ae0d6e671b34c0f0cba7bbbcd1624128e0c09e3
Dyskal <yoshilexis@gmail.com>
2021-09-15 09:18:17 +0200

    update User

--- User.php
+++ User.php
@@ -1,4 +1,5 @@
 <?php
+
 class User {
     private $lname;
     private $fname;
@@ -9,11 +10,12 @@ class User {
     private $height;
     private $weight;
 
-    public function  __construct() { }
-    public function init($n,$p,$passwd,$email,$bdate,$gender,$height,$weight){
-        $this->lname = $n;
-        $this->fname = $p;
-        $this->password = $passwd;
+    public function __construct() {}
+
+    public function init($lname, $fname, $password, $email, $bdate, $gender, $height, $weight) {
+        $this->lname = $lname;
+        $this->fname = $fname;
+        $this->password = $password;
         $this->email = $email;
         $this->bdate = $bdate;
         $this->gender = $gender;
@@ -21,15 +23,41 @@ class User {
         $this->weight = $weight;
     }
 
-    public function getLastname(){ return $this->lname; }
-    public function getFirstname(){ return $this->fname; }
-    public function getPassword(){ return $this->password; }
-    public function getEmail(){ return $this->email; }
-    public function getBirthDate(){ return $this->bdate; }
-    public function getGender(){ return $this->gender; }
-    public function getHeight(){ return $this->height; }
-    public function getWeight(){ return $this->weight; }
+    public function getLastname() {
+        return $this->lname;
+    }
 
-    public function  __toString() { return $this->lname. " ". $this->fname. " ". $this->password. " ". $this->email. " ". $this->bdate. " ". $this->gender. " ". $this->height. " ". $this->weight; }
+    public function getFirstname() {
+        return $this->fname;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getBirthDate() {
+        return $this->bdate;
+    }
+
+    public function getGender() {
+        return $this->gender;
+    }
+
+    public function getHeight() {
+        return $this->height;
+    }
+
+    public function getWeight() {
+        return $this->weight;
+    }
+
+    public function __toString() {
+        return $this->lname . " " . $this->fname . " " . $this->password . " " . $this->email . " " . $this->bdate . " " . $this->gender . " " . $this->height . " " . $this->weight;
+    }
 }
+
 ?>
