f73dda27f0b6a4da5d7ffa3ffa0a2e5330220b5b
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2021-09-15 08:38:28 +0200

    Update User.php

--- User.php
+++ User.php
@@ -21,15 +21,15 @@
      $this->weight = $weight;
    }
 
-   public function getLastname(){ return $this->nom; }
-   public function getFirstname(){ return $this->prenom; }
+   public function getLastname(){ return $this->lname; }
+   public function getFirstname(){ return $this->fname; }
    public function getPassword(){ return $this->password; }
    public function getEmail(){ return $this->email; }
    public function getBirthDate(){ return $this->bdate; }
-   public function getPassword(){ return $this->password; }
-   public function getPassword(){ return $this->password; }
-   public function getPassword(){ return $this->password; }
+   public function getGender(){ return $this->gender; }
+   public function getHeight(){ return $this->height; }
+   public function getWeight(){ return $this->weight; }
 
-   public function  __toString() { return $this->nom. " ". $this->prenom; }
+   public function  __toString() { return $this->lname. " ". $this->fname. " ". $this->password. " ". $this->email. " ". $this->bdate. " ". $this->gender. " ". $this->height. " ". $this->weight; }
   }
 ?>
