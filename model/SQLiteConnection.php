f28a6dd7274baea630a6ea1deeab96d2a30e422d
Dyskal <yoshilexis@gmail.com>
2021-09-15 08:39:42 +0200

    add SQLiteConnection

---
+++ model/SQLiteConnection.php
@@ -0,0 +1,23 @@
+<?php
+class SQLiteConnection {
+    private static $instance;
+
+    private function __construct() {}
+
+    public final static function getInstance() {
+        if(!isset(self::$instance)) {
+            self::$instance = new SQLiteConnection();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        try {
+            $conn = new PDO("sqlite:" . dirname(__FILE__) . "/../sql/sport_track.db");
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            print("Error: " . $e->getMessage() . "<br/>");
+            die();
+        }
+    }
+}
