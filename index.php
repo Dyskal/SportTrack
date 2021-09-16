1b29a19b7eb8196c8656b118c2eb31107356fb11
Dyskal <yoshilexis@gmail.com>
2021-09-16 12:45:11 +0200

    reformat code & add index

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
     <meta charset="UTF-8">
     <title>SportTrack | Accueil</title>
     <link href="./style/style.css" rel="stylesheet">
-    <link href="./img/logo.svg" rel="icon" />
+    <link href="./img/logo.svg" rel="icon"/>
 </head>
 
 <body>
-    <header>
+<header>
     <h1 onclick="window.location.href='./'">SportTrack</h1>
-    </header>
-    <div class=accueil>
+</header>
+<div class=accueil>
     <button class=button onclick="window.location.href='./profile.php'">Profile</button>
     <button class=button onclick="window.location.href='./login.php'">Login</button>
     <button class=button onclick="window.location.href='./register.php'">Register</button>
     <button class=button onclick="window.location.href='./upload.php'">Upload</button>
-    </div>
+</div>
 </body>
 
 </html>
