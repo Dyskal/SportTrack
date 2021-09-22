1fa2ae71ddf10889ce67ea8efdc6fdceef321a6f
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2021-09-22 09:07:45 +0200

    add controllers

--- controller/AddUserController.php
+++ controller/AddUserController.php
@@ -1,8 +1,27 @@
 <?php
 
+
+require("../model/UserDAO.php");
+require ("Controller.php");
+
 class AddUserController implements Controller {
+    public function __construct() {
+
+        $this->AddUser();
+    }
 
     public function handle($request) {
         // TODO: Implement handle() method.
     }
+
+    public function AddUser() {
+        $User = new User();
+        $User->init($_POST["mail"], $_POST["password"], $_POST["lname"], $_POST["fname"], $_POST["date"], $_POST["sex"], $_POST["height"], $_POST["weight"]);
+        $UserDAO = UserDAO::getInstance();
+        $UserDAO->insert($User);
+
+    }
+
 }
+var_dump($_POST);
+$o = new AddUserController();
--- view/register.php
+++ view/register.php
@@ -14,7 +14,7 @@
 </header>
 <div class="container">
     <h2>Register</h2>
-    <form>
+    <form action="./controller/AddUserController.php" method="post">
         <label for="fname">First name:</label><br>
         <input class="input" id="fname" name="fname" required type="text"/><br>
         <label for="lname">Last name:</label><br>
@@ -24,14 +24,14 @@
         <label>Gender:</label><br>
         <div class="container-radio">
             <div>
-                <input checked class="radio" id="w" name="sex" required type="radio"/>
+                <input checked class="radio" id="w" value="W" name="sex" required type="radio"/>
                 <label for="w">Woman</label></div>
             <div>
-                <input class="radio" id="m" name="sex" type="radio">
+                <input class="radio" id="m" value="M" name="sex" type="radio">
                 <label for="m">Man</label>
             </div>
             <div>
-                <input class="radio" id="o" name="sex" type="radio">
+                <input class="radio" id="o" value="O" name="sex" type="radio">
                 <label for="o">Other</label>
             </div>
         </div>
