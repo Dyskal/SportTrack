d0892e7e86f317dc99d96a3d786aa5ea3e24d6da
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2021-09-22 16:05:54 +0200

    add charging animation

--- controller/AddUserController.php
+++ controller/AddUserController.php
@@ -19,9 +19,19 @@ class AddUserController implements Controller {
         $User->init($_POST["mail"], $_POST["password"], $_POST["lname"], $_POST["fname"], $_POST["date"], $_POST["sex"], $_POST["height"], $_POST["weight"]);
         $UserDAO = UserDAO::getInstance();
         $UserDAO->insert($User);
-
+        ?>
+        <script type="text/javascript">
+            window.location.href = '..';
+        </script>
+        <head>
+            <meta charset="UTF-8">
+            <title>SportTrack | Accueil</title>
+            <link href="../style/style.css" rel="stylesheet">
+            <link href="../img/logo.svg" rel="icon"/>
+        </head>
+        <div class=loading-content><div class=loading1></div><div class=loading2></div><div class=loading3></div></div>
+        <?php
     }
 
 }
-var_dump($_POST);
 $o = new AddUserController();
