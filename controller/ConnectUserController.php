ec30e8779d8f2bdc1807753edd7edac94c8171db
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2021-09-23 11:47:44 +0200

    jsp se que j'ai fais

--- controller/ConnectUserController.php
+++ controller/ConnectUserController.php
@@ -1,8 +1,15 @@
 <?php
+require("../model/UserDAO.php");
+require("Controller.php");
 
 class ConnectUserController implements Controller {
 
     public function handle($request) {
         // TODO: Implement handle() method.
+    }
+
+    public function connect() {
+        session_start();
+        $_SESSION["email"] = "test";
     }
 }
