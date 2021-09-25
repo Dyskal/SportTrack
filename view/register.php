a7337673a8bb9d2379d52a8d09423707a2c4e4a7
Dyskal <yoshilexis@gmail.com>
2021-09-25 19:34:54 +0200

    reformat a lot of code

--- view/register.php
+++ view/register.php
@@ -12,16 +12,13 @@
 <header>
     <h1 onclick="window.location.href='./'">SportTrack</h1>
     <button onclick="burgerMenu()" id="header-burger" class="header-btn header-burger"></button>
-
     <nav id="menu">
         <button onclick="window.location.href='./'" class="header-btn header-home"></button>
-
         <button onclick="window.location.href='?page=register'" class="header-btn header-login">Register</button>
         <button onclick="window.location.href='?page=login'" class="header-btn header-login">Login</button>
-
-
     </nav>
 </header>
+
 <div class="container">
     <h2>Register</h2>
     <form action="./controller/AddUserController.php" method="post">
@@ -35,33 +32,31 @@
         <input class="input" id="fname" name="fname" required type="text"/>
         <label for="bdate">Birth date</label>
         <input class="input" id="bdate" name="bdate" required type="date"/>
-        <label>Gender</label>
-<!--        <div class="container-radio">-->
-<!--            <div>-->
-<!--                <input checked class="radio" id="w" value="W" name="gender" required type="radio"/>-->
-<!--                <label for="w">Woman</label></div>-->
-<!--            <div>-->
-<!--                <input class="radio" id="m" value="M" name="gender" type="radio">-->
-<!--                <label for="m">Man</label>-->
-<!--            </div>-->
-<!--            <div>-->
-<!--                <input class="radio" id="o" value="O" name="gender" type="radio">-->
-<!--                <label for="o">Other</label>-->
-<!--            </div>-->
-<!--        </div>-->
-
-
-        <select  name="gender">
+        <label for="gender">Gender</label>
+        <!--        <div class="container-radio">-->
+        <!--            <div>-->
+        <!--                <input checked class="radio" id="w" value="W" name="gender" required type="radio"/>-->
+        <!--                <label for="w">Woman</label></div>-->
+        <!--            <div>-->
+        <!--                <input class="radio" id="m" value="M" name="gender" type="radio">-->
+        <!--                <label for="m">Man</label>-->
+        <!--            </div>-->
+        <!--            <div>-->
+        <!--                <input class="radio" id="o" value="O" name="gender" type="radio">-->
+        <!--                <label for="o">Other</label>-->
+        <!--            </div>-->
+        <!--        </div>-->
+        <select name="gender" id="gender">
             <option value="M">Man</option>
             <option value="W">Women</option>
             <option value="O">Other</option>
         </select>
         <label for="height">Height</label>
         <input class="input" id="height" min="0" name="height" oninput="validity.valid||(value='');" required
-        type="number"/>
+               type="number"/>
         <label for="weight">Weight</label>
         <input class="input" id="weight" min="0" name="weight" oninput="validity.valid||(value='');" required
-        type="number"/>
+               type="number"/>
         <input class="button right" type="submit" value="Submit"/>
     </form>
 </div>
