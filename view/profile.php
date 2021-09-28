9384c5ce592682f67b64d251f3d9a8a685bc7d99
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2021-09-28 23:14:13 +0200

    responsive

--- view/profile.php
+++ view/profile.php
@@ -4,6 +4,11 @@
 session_start()
 ?>
 <head>
+    <meta name="viewport" content="width=device-width, maximum-scale=1.5">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta charset="UTF-8">
     <title>SportTrack | Profile</title>
     <link href="./style/style.css" rel="stylesheet">
