f1209a6299adddea3bf04c57eb273de6d960c8ac
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2021-09-29 16:06:28 +0200

    change table style

--- index.php
+++ index.php
@@ -74,6 +74,11 @@ if (isset($_SESSION['email'])) {
         let nav = document.getElementById("menu");
         nav.classList.toggle("toggle");
         document.getElementById("header-burger").classList.toggle("toggle");
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+
+
+        document.body.classList.toggle("toggle");
     }
 </script>
 </body>
