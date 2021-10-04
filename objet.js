ea7ffd715687ed220e68ce6b368557a2e67ca5fe
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2021-10-04 10:05:53 +0200

    add charging animation

---
+++ objet.js
@@ -0,0 +1,13 @@
+
+import { calculDistance2PointsGPS } from './fonctions.js';
+
+function CalculDistance(){
+
+}
+
+CalculDistance.prototype.calculDistance2PointsGPS = function (lat1,long1,lat2,long2) {
+    calculDistance2PointsGPS(lat1,long1,lat2,long2)
+};
+CalculDistance.prototype.calculDistanceTrajet = function (lat1,long1,lat2,long2) {
+
+};
