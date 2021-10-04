96f289dab65c4ff96e2b9f50eec1e17145cabd96
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2021-10-04 10:17:31 +0200

    uho

--- objet.js
+++ objet.js
@@ -1,13 +1,15 @@
 
 import { calculDistance2PointsGPS } from './fonctions.js';
+import { calculDistanceTrajet } from './fonctions.js';
 
-function CalculDistance(){
-
-}
+function CalculDistance() {};
 
 CalculDistance.prototype.calculDistance2PointsGPS = function (lat1,long1,lat2,long2) {
     calculDistance2PointsGPS(lat1,long1,lat2,long2)
 };
-CalculDistance.prototype.calculDistanceTrajet = function (lat1,long1,lat2,long2) {
-
+CalculDistance.prototype.calculDistanceTrajet = function (parcours) {
+    calculDistanceTrajet();
 };
+
+
+console.log(calculDistanceTrajet(array))
