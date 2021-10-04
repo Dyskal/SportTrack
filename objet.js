75f8b33d9670b374f88af30101eaebe49be07d99
Dyskal <yoshilexis@gmail.com>
2021-10-04 10:25:26 +0200

    update objet.js

--- objet.js
+++ objet.js
@@ -1,15 +1,15 @@
-
 import { calculDistance2PointsGPS } from './fonctions.js';
 import { calculDistanceTrajet } from './fonctions.js';
+import { array } from './fonctions.js';
 
-function CalculDistance() {};
+function CalculDistance() {}
 
-CalculDistance.prototype.calculDistance2PointsGPS = function (lat1,long1,lat2,long2) {
-    calculDistance2PointsGPS(lat1,long1,lat2,long2)
+CalculDistance.prototype.calculDistance2PointsGPS = function (lat1, long1, lat2, long2) {
+    return calculDistance2PointsGPS(lat1,long1,lat2,long2)
 };
 CalculDistance.prototype.calculDistanceTrajet = function (parcours) {
-    calculDistanceTrajet();
+    return calculDistanceTrajet(parcours);
 };
 
-
-console.log(calculDistanceTrajet(array))
+let calcul = new CalculDistance()
+console.log(calcul.calculDistanceTrajet(array))
