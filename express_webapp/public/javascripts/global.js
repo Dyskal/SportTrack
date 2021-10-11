ce16757dd7dab47f278dfccc0f788adc57471239
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2021-10-11 12:34:56 +0200

    add notif to jade

--- express_webapp/public/javascripts/global.js
+++ express_webapp/public/javascripts/global.js
@@ -9,27 +9,7 @@ function burgerMenu() {
 
 
 
-var url = new URL(window.location.href);
-var color = url.searchParams.get("color");
-var msg = url.searchParams.get("msg");
-var newDiv = document.createElement("div");
-newDiv.classList.add("error");
-if (color != null) {
-    newDiv.style.background = color;
-    console.log("test");
 
-}
-console.log("test");
-
-if (msg != null) {
-    console.log("test");
-
-    newDiv.style.content = msg;
-    var newContent = document.createTextNode(msg);
-    newDiv.appendChild(newContent);
-}
-
-document.body.insertBefore(newDiv, document.getElementsByTagName("div")[0]);
 
 
 
@@ -38,17 +18,3 @@ setTimeout(function() {
 }, 10000)
 
 
-// <?php
-// if (isset($_GET['msg'])) {
-//     $error = urldecode($_GET['msg']);
-//     if (strlen($error) > 1) {
-//             ?>
-//     <div style="background: <?php if (isset($_GET['color'])) {
-//         echo $_GET['color'];
-//     } ?>" class="error" id="hideDiv">
-//     <?php echo(htmlspecialchars($error)); ?>
-// </div>
-//     <?php
-// }
-// }
-//     ?>
