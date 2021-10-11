f00d3f0c48fc4bf02ab1c8e71cdccaaf5465bdfc
Dyskal <yoshilexis@gmail.com>
2021-10-11 12:49:42 +0200

    update views

--- express_webapp/routes/register.js
+++ express_webapp/routes/register.js
@@ -10,8 +10,8 @@ router.get('/', asyncMiddleware(async (req, res, next) => {
 router.post('/', asyncMiddleware(async (req, res, next) => {
     const usr = await user_dao.findByKey(req.body.email)
     if (usr.length === 0) {
-        if (req.body.email != null && req.body.password != null && req.body.lname != null && req.body.fname != null && req.body.bdate != null &&
-            req.body.gender != null && req.body.height != null && req.body.weight != null) {
+        if (req.body.email != null && req.body.password != null && req.body.lname != null && req.body.fname != null &&
+            req.body.bdate != null && req.body.gender != null && req.body.height != null && req.body.weight != null) {
             await user_dao.insert(req.body)
             res.render('login', {error: false, fromregister: true})
         }
