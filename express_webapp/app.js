db2468a9b2d68ee5b3e47e70336ee2e035b0d3a4
Dyskal <yoshilexis@gmail.com>
2021-10-24 15:02:54 +0200

    fix 404 error

--- express_webapp/app.js
+++ express_webapp/app.js
@@ -41,7 +41,7 @@ app.use('/upload', upload);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    // next(createError(404));
+    createError(404);
     res.sendStatus(404);
 });
 
